10f94889f9d6f0dc31c46bd843a20875bccb7b19
Startklar <wi22b033@technikum-wien.at>
2023-07-02 20:25:58 +0200

    Initial commit

---
+++ src/main/java/Maintance_Monitor/Monitor_SLM/MonitorSlmApplication.java
@@ -0,0 +1,13 @@
+package Maintance_Monitor.Monitor_SLM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorSlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorSlmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Maintance_Monitor/Monitor_SLM/MonitorSlmApplicationTests.java
@@ -0,0 +1,13 @@
+package Maintance_Monitor.Monitor_SLM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorSlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
